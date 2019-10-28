04a976b4a14fadb53d09972cc98fbf756aab2fb7
Zach Seaman <znseaman@gmail.com>
2019-10-27 17:51:48 -0700

    Add ellipsises for overflow text Add flexbox for layout of places

--- client/src/components/Place.js
+++ client/src/components/Place.js
@@ -5,8 +5,38 @@ import Pin from "./Pin";
 
 const Place = ({ place }) => {
   return (
-    <div>
-      <h2>{place.name}</h2>
+    <div
+      style={{
+        width: "500px",
+        height: "150px",
+        margin: "auto",
+        // flexbox the rest as well
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        // styling
+        border: "1px solid #333",
+        padding: "1rem"
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column"
+        }}
+      >
+        <h3>{place.name}</h3>
+        <p
+          className="truncate-overflow"
+          style={{
+            maxWidth: "250px",
+            // allow for appending "..." for text overflow
+            position: "relative"
+          }}
+        >
+          {place.description}
+        </p>
+      </div>
       <Map center={[place.longitude, place.latitude]}>
         <Marker
           key={place.id}
--- client/src/components/Places.js
+++ client/src/components/Places.js
@@ -1,16 +1,21 @@
-import React, {useState} from 'react'
-import PLACES from '../data/places'
-import Place from './Place'
+import React, { useState } from "react";
+import PLACES from "../data/places";
+import Place from "./Place";
 
 const Places = () => {
-    const [places, setPlaces] = useState(PLACES)
-    return (
-        <div>
-            {places.map(place => (
-                <Place key={place.id} place={place}></Place>
-            ))}
-        </div>
-    )
-}
+  const [places, setPlaces] = useState(PLACES);
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column"
+      }}
+    >
+      {places.map(place => (
+        <Place key={place.id} place={place}></Place>
+      ))}
+    </div>
+  );
+};
 
-export default Places
+export default Places;
