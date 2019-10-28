035bfc684313f1cbf426948f69efeede290a66b1
Zach Seaman <znseaman@gmail.com>
2019-10-27 17:52:55 -0700

    Add descriptions to places data

--- client/src/data/places.js
+++ client/src/data/places.js
@@ -2,24 +2,32 @@ const places = [
   {
     id: 1,
     name: "Banff National Park",
+    description:
+      "Banff National Park is Canada's oldest national park and was established in 1885. Located in the Rocky Mountains, 110–180 kilometres (68–112 mi) west of Calgary in the province of Alberta, Banff encompasses 6,641 square kilometres (2,564 sq mi) of mountainous terrain, with many glaciers and ice fields, dense coniferous forest, and alpine landscapes. The Icefields Parkway extends from Lake Louise, connecting to Jasper National Park in the north. Provincial forests and Yoho National Park are neighbours to the west, while Kootenay National Park is located to the south and Kananaskis Country to the southeast. The main commercial centre of the park is the town of Banff, in the Bow River valley.",
     latitude: 51.5,
     longitude: -116
   },
   {
     id: 2,
     name: "Palenque",
+    description:
+      'Palenque, also anciently known as Lakamha (literally: "Big Water"), was a Maya city state in southern Mexico that flourished in the 7th century. The Palenque ruins date from ca. 226 BC to ca. AD 799. After its decline, it was absorbed into the jungle of cedar, mahogany, and sapodilla trees, but has since been excavated and restored. It is located near the Usumacinta River in the Mexican state of Chiapas, about 130 km (81 mi) south of Ciudad del Carmen, 150 m (164 yd) above sea level. It averages a humid 26 °C (79 °F) with roughly 2160 mm (85 in) of rain a year.',
     latitude: 17.483978,
     longitude: -92.046328
   },
   {
     id: 3,
     name: "Tokyo",
+    description:
+      'Tokyo is one of the 47 prefectures of Japan. It has served as the Japanese capital since 1869. As of 2018, the Greater Tokyo Area ranked as the most populous metropolitan area in the world. The urban area houses the seat of the Emperor of Japan, of the Japanese government and of the National Diet. Tokyo forms part of the Kantō region on the southeastern side of Japan\'s main island, Honshu, and includes the Izu Islands and Ogasawara Islands. Tokyo was formerly named Edo when Shōgun Tokugawa Ieyasu made the city his headquarters in 1603. It became the capital after Emperor Meiji moved his seat to the city from Kyoto in 1868; at that time Edo was renamed Tokyo. The Tokyo Metropolis formed in 1943 from the merger of the former Tokyo Prefecture (東京府 Tōkyō-fu) and the city of Tokyo (東京市 Tōkyō-shi). Tokyo is often referred to as a city but is officially known and governed as a "metropolitan prefecture", which differs from and combines elements of a city and a prefecture, a characteristic unique to Tokyo.',
     latitude: 35.689722,
     longitude: 139.692222
   },
   {
     id: 4,
     name: "Curaçao",
+    description:
+      "Curaçao is a Lesser Antilles island in the southern Caribbean Sea and the Dutch Caribbean region, about 65 km (40 mi) north of the Venezuelan coast. It is a constituent country (Dutch: land) of the Kingdom of the Netherlands.",
     latitude: 12.183333,
     longitude: -69
   }
