b40fb4a319cfe2574e8cf76d477fd3c9b70c6a06
Zach Seaman <znseaman@gmail.com>
2019-11-01 20:57:20 -0700

    impr: add font-awesome icon to Menu & move photo

--- client/src/components/AddPlace.js
+++ client/src/components/AddPlace.js
@@ -4,42 +4,45 @@ import v4 from "uuid/v4";
 
 const AddPlaceForm = ({ places, setPlaces }) => {
   /* @TODO: try using lens & only 1 object for the form in useState */
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [photo, setPhoto] = useState("");
-  const [latitude, setLatitude] = useState("");
-  const [longitude, setLongitude] = useState("");
+  const initialFormData = {
+    name: "",
+    description: "",
+    photo: "",
+    latitude: "",
+    longitude: ""
+  };
+  const [formData, setFormData] = useState(initialFormData);
 
   const onChange = e => {
     const { name, value } = e.target;
     switch (name) {
-      case "name":
-        setName(value);
-        break;
-      case "description":
-        setDescription(value);
-        break;
-      case "photo":
-        setPhoto(value);
-        break;
-      case "latitude":
-        /* @TODO: Add checks if number is actually possible to be a latitude */
-        setLatitude(+value);
-        break;
       case "longitude":
-        /* @TODO: Add checks if number is actually possible to be a longitude */
-        setLongitude(+value);
+      case "latitude":
+        /* @TODO: Add checks if number is actually possible to be a latitude or longitude*/
+        setFormData({
+          ...formData,
+          [name]: value
+        });
+        break;
+      default:
+        setFormData({
+          ...formData,
+          [name]: value
+        });
         break;
     }
   };
 
-  const resetFields = () => {
-    const value = "";
-    setName(value);
-    setDescription(value);
-    setPhoto(value);
-    setLatitude(value);
-    setLongitude(value);
+  const onClick = e => {
+    e.preventDefault();
+    const hasImage = formData["photo"] !== "";
+    const newPlace = {
+      id: v4(),
+      hasImage,
+      ...formData
+    };
+    setPlaces([newPlace, ...places]);
+    setFormData(initialFormData);
   };
 
   return (
@@ -49,55 +52,38 @@ const AddPlaceForm = ({ places, setPlaces }) => {
           label="Name"
           control="input"
           name="name"
-          value={name}
+          value={formData["name"]}
           onChange={onChange}
         />
         <FormField
           label="Description"
           control="input"
           name="description"
-          value={description}
+          value={formData["description"]}
           onChange={onChange}
         />
         <FormField
           label="Photo Url"
           control="input"
           name="photo"
-          value={photo}
+          value={formData["photo"]}
           onChange={onChange}
         />
         <FormField
           label="Latitude"
           control="input"
           name="latitude"
-          value={latitude}
+          value={formData["latitude"]}
           onChange={onChange}
         />
         <FormField
           label="Longitude"
           control="input"
           name="longitude"
-          value={longitude}
+          value={formData["longitude"]}
           onChange={onChange}
         />
-        <Button
-          onClick={e => {
-            e.preventDefault();
-            const hasImage = photo !== "";
-            const newPlace = {
-              id: v4(),
-              name,
-              description,
-              photo,
-              hasImage,
-              latitude: latitude,
-              longitude: longitude
-            };
-            setPlaces([newPlace, ...places]);
-            resetFields();
-          }}
-          primary
-        >
+        <Button onClick={onClick} primary>
           Submit
         </Button>
       </Form.Group>
--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Container, Image, Menu, MenuItem } from "semantic-ui-react";
+import { faGlobeAmericas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function MenuComponent() {
   return (
     <Menu>
       <Container>
         <MenuItem as="a" header>
-          <Image
-            src="https://media.licdn.com/dms/image/C4E03AQF1a3nJPjpMow/profile-displayphoto-shrink_100_100/0?e=1577923200&v=beta&t=mYeQh7WmLGu7m9YRtYHKi6nzaTF-F_c7B-1MsWZE8-c"
-            width="68"
-            height="68"
-            circular
-          />
+          <FontAwesomeIcon icon={faGlobeAmericas} size="5x"></FontAwesomeIcon>
+        </MenuItem>
+
+        <MenuItem as="h3" header>
+          To Go To List
         </MenuItem>
 
         <Menu.Menu position="right">
@@ -21,6 +22,14 @@ export default function MenuComponent() {
 
           <MenuItem as="a" name="register">
             Register
+          </MenuItem>
+          <MenuItem as="a" header>
+            <Image
+              src="https://media.licdn.com/dms/image/C4E03AQF1a3nJPjpMow/profile-displayphoto-shrink_100_100/0?e=1577923200&v=beta&t=mYeQh7WmLGu7m9YRtYHKi6nzaTF-F_c7B-1MsWZE8-c"
+              width="68"
+              height="68"
+              circular
+            />
           </MenuItem>
         </Menu.Menu>
       </Container>
