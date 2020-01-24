a9e128c4a8e2c8b68bf421dc5e41df22a94aab8a
Zach Seaman <znseaman@gmail.com>
2020-01-24 08:18:26 +0000

    deploy: fix port number

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -5,7 +5,7 @@ const baseConfig = {
   env,
   isDev: env == 'development',
   isTest: env === 'testing',
-  port: 5000,
+  port: 4000,
   secrets: {
     jwt: process.env.JWT_SECRET,
     jwtExp: Number(process.env.JWT_EXP)
