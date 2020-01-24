8bb1d625c9ba3b82279916e0c5bebfbe40a216d7
Zach Seaman <znseaman@gmail.com>
2020-01-23 23:01:08 -0800

    improv(dx): set baseURL directly from env var

--- client/src/shared/axios.js
+++ client/src/shared/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { getItem, removeItem } from './sessionStorage';
-const baseURL = process.env.NODE_ENV == 'development' ? process.env.REACT_APP_BASE_URL : '';
+const baseURL = process.env.REACT_APP_BASE_URL;
 
 const instance = axios.create({
   baseURL,
